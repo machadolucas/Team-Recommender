a1252f393e23b2cffad9dd65d58309e7601231e0
Lucas Machado <machadolucas@me.com>
2019-04-15 01:03:58 +0300

    Initial tests

--- main.py
+++ main.py
@@ -2,65 +2,88 @@ import itertools
 import matplotlib.pyplot as plt
 import networkx as nx
 import time
+from collections import defaultdict
+
+'''
+Number of applicants: 7428
+Number of skills: 4480
+7428*7427*7426*7425
+3041842488715800
+avg 0.0001 s per team score calc
+304184248871.5800 s = 9639 years
+
+An applicant 'a' is a tuple in the following format:
+a = (identifier, {skill1, skill2, ...})
+
+Likewise, a project 'p' is a tuple in the following format:
+a = (identifier, {skill1, skill2, ...})
+
+Skills of the applicant or project are used in the algorithm
+ by their numerical identifiers such as 0,1,2,...
+'''
+set_A = set()
+skills_dict = {}
+
+
+def get_set_a():
+    # Read skills file and create applicants tuples in set_A
+    applicants_data = []
+    with open("DBLP_skill.csv", 'r', encoding='latin1') as temp_f:
+        lines = temp_f.readlines()
+        for index, line in enumerate(lines):
+            split_line = line.rstrip('\n').split(',')
+            if len(split_line) > 1:
+                applicants_data.append((index, frozenset(split_line[1:])))
+    temp_f.close()
+
+    # Gets frequency of terms
+    freq_skills = defaultdict(int)
+    for applicant in applicants_data:
+        applicant_skills = applicant[1]
+        for skill in applicant_skills:
+            freq_skills[skill] += 1
+
+    # Builds a skill dictionary with ids instead of the terms for faster processing TODO needed?
+    for index, term in enumerate(freq_skills.keys()):
+        skills_dict[index] = term
+    freq_skills_id = {}
+    for sid, term in skills_dict.items():
+        freq_skills_id[sid] = freq_skills[term]
+
+    # Recreates applicants_data as set_A with ids instead of strings
+    for applicant in applicants_data:
+        set_A.add((applicant[0], frozenset([list(skills_dict.values()).index(skill) for skill in applicant[1]])))
+
+    # Gets 2k most frequent terms from all the 4480 terms
+    most_freq = sorted(freq_skills_id.items(), key=lambda item: item[1], reverse=True)[:2000]
+
 
 # Creates a tree (graph) with the identifiers of the skills and their relations
-G = nx.random_tree(15)  # TODO
+G = nx.random_tree(2000)  # TODO
 
 # Pre compute all shortest distances between skills in the tree
 distances = dict(nx.all_pairs_shortest_path_length(G))
 
-# Plots the graph
-plt.plot()
-nx.draw(G, with_labels=True, font_weight='bold')
-plt.show()
 
-'''
-An applicant 'a' is a tuple in the following format:
-a = (identifier, [skill1, skill2, …])
-
-Likewise, a project 'p' is a tuple in the following format:
-a = (identifier, [skill1, skill2, …])
-
-Skills of the applicant or project are used in the algorithm
- by their numerical identifiers such as 0,1,2,…
-'''
-set_A = [  # TODO
-    ('', [0, 1])
-]
-set_P = [  # TODO
-    ('Project 1', [0, 1]),
-    ('Project 2', [0, 4]),
-    ('Project 3', [2, 3, 1]),
-]
-skills = {  # TODO
-    1: "",
-    2: "",
-    3: "",
-    4: "",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: "",
-    10: "",
-    11: "",
-    12: "",
-    13: "",
-    14: "",
-    15: "",
-    16: "",
-    17: "",
-    18: "",
-    19: "",
-    20: "",
-}
+def get_set_p():
+    set_p = frozenset([
+        ('Project 1', frozenset([list(skills_dict.values()).index(skill) for skill in
+                                 ['learning', 'networks', 'knowledge', 'study', 'graphs', 'analysis']])),
+        ('Project 2', frozenset([list(skills_dict.values()).index(skill) for skill in
+                                 ['learning', 'fuzzy', 'algorithms', 'networks', 'classification', 'analysis']])),
+        ('Project 3', frozenset([list(skills_dict.values()).index(skill) for skill in
+                                 ['graphs', 'clustering', 'distributed', 'algorithms', 'analysis']])),
+        ('Project 4', frozenset([list(skills_dict.values()).index(skill) for skill in
+                                 ['graphs', 'clustering', 'distributed', 'algorithms', 'analysis']])),
+    ])
+    return set_p
 
 
 # Similarity function
 def similarity(s1, s2):
     if s1 == s2:
         return 1
-    elif nx.is_simple_path(G, [s2, s2]):
+    elif s1 < 2000 and s2 < 2000 and nx.is_simple_path(G, [s1, s2]):
         return 1 / distances[s1][s2]
     else:
         return 0
@@ -97,10 +120,13 @@ def brute_force_teams(applicants, projects, k):
         possible_teams = itertools.combinations(applicants, k)
         team_scores = {}
         for team in possible_teams:
+            start = time.time()
             score_team = score_tp(team, project)
             team_scores[team] = score_team
+            start_end = time.time()
+            print(start_end - start)
         best_team_for_project = max(team_scores, key=team_scores.get)
-        best_teams[project] = best_team_for_project
+        best_teams[project] = (best_team_for_project, team_scores[best_team_for_project])
         for member in best_team_for_project:
             applicants.remove(member)
     return best_teams
@@ -125,8 +151,8 @@ def single_heuristic_teams(applicants, projects, k):
                 best_teams[project].append(best_applicant)
             else:
                 best_teams[project] = [best_applicant]
-            project_applicants_scores = {project: val for project, val in project_applicants_scores.items()
-                                         if val[0] != best_applicant}
+            project_applicants_scores = {project: [a for a in lst if a != best_applicant]
+                                         for project, lst in project_applicants_scores.items()}
     return best_teams
 
 
@@ -149,41 +175,79 @@ def pair_heuristic_teams(applicants, projects, k):
                 best_teams[project].append(best_applicant)
             else:
                 best_teams[project] = [best_applicant]
-            project_applicants_scores = {project: val
-                                         for project, val in project_applicants_scores.items()
-                                         if val[0] != best_applicant}
+            project_applicants_scores = {project: [a for a in lst if a != best_applicant]
+                                         for project, lst in project_applicants_scores.items()}
             if m < k:
                 project_available.remove((best_applicant[0], best_applicant[1]))  # Is this line needed?
                 snd_best_applicant = max(project_available, key=lambda item: item[1])
-                project_applicants_scores = {project: val
-                                             for project, val in project_applicants_scores.items()
-                                             if val[0] != snd_best_applicant}
+                best_teams[project].append(snd_best_applicant)
+                project_applicants_scores = {project: [a for a in lst if a != snd_best_applicant]
+                                             for project, lst in project_applicants_scores.items()}
     return best_teams
 
 
 def print_results():
+    get_set_a()
+    set_p = get_set_p()
+    '''
+    print('Calculating brute force method...')
     start_brute_force = time.time()
-    res_bf = brute_force_teams(set_A, set_P, 6)
+    res_bf = brute_force_teams(set_A, set_p, 6)
     end_brute_force = time.time()
     time_brute_force = end_brute_force - start_brute_force
+    '''
 
+    print('Input: --------------')
+    for project in set_p:
+        print('\t"{}",\trequirements:"{}"'.format(project[0], [skills_dict[skill] for skill in project[1]]))
+    print('\tAmount of applicants: {}'.format(len(set_A)))
+    print('\tAmount of different skills: {}'.format(len(skills_dict)))
+
+    print('\nCalculating single heuristic method...')
     start_single_heuristic = time.time()
-    res_sh = single_heuristic_teams(set_A, set_P, 6)
+    res_sh = single_heuristic_teams(set_A, set_p, 6)
     end_single_heuristic = time.time()
     time_single_heuristic = end_single_heuristic - start_single_heuristic
 
+    print('\nCalculating pair heuristic method...')
     start_pair_heuristic = time.time()
-    res_ph = pair_heuristic_teams(set_A, set_P, 6)
+    res_ph = pair_heuristic_teams(set_A, set_p, 6)
     end_pair_heuristic = time.time()
     time_pair_heuristic = end_pair_heuristic - start_pair_heuristic
 
-    print('Results:')
+    print('\n\nResults:')
 
+    '''
     print('Brute force: -------------------')
-    print('\tExecution time:\t{}'.format(time_brute_force))
-
+    print('Execution time:\t{}'.format(time_brute_force))
+    for project, team in res_bf:
+        print('\tTeam for project "{}",\trequirements:{},\tscoreTP:{}'
+              .format(project[0], [skills_dict[skill] for skill in project[1]], team[1]))
+        for member in team[0]:
+            print('\t\t"{}",\tskills:{},\tscoreAP:{}'
+                  .format(member[0], [skills_dict[skill] for skill in member[1]], score_ap(member, project)))
+    '''
     print('Single heuristic: --------------')
-    print('\tExecution time:\t{}'.format(time_single_heuristic))
+    print('Execution time:\t{}'.format(time_single_heuristic))
+    for project, team in res_sh.items():
+        print('\t->"{}":\tscoreTP:{},\trequirements:{}'
+              .format(project[0], score_tp([m[0] for m in team], project),
+                      [skills_dict[skill] for skill in project[1]]))
+        for member in team:
+            print('\t\t-{}:\tscoreAP:{}\tskills:{}'
+                  .format(member[0][0], member[1], [skills_dict[skill] for skill in member[0][1]]))
 
     print('Pair heuristic: ----------------')
-    print('\tExecution time:\t{}'.format(time_pair_heuristic))
+    print('Execution time:\t{}'.format(time_pair_heuristic))
+    for project, team in res_ph.items():
+        print('\t->"{}":\tscoreTP:{},\trequirements:{}'
+              .format(project[0], score_tp([m[0] for m in team], project),
+                      [skills_dict[skill] for skill in project[1]]))
+        for member in team:
+            print('\t\t-{}:\tscoreAP:{}\tskills:{}'
+                  .format(member[0][0], member[1], [skills_dict[skill] for skill in member[0][1]]))
+
+
+# Execute algorithms and print results
+if __name__ == "__main__":
+    print_results()
