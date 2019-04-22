8a0bc863a2ad260e1e9f7daf1ceae41b87511ca9
Lucas Machado <machadolucas@me.com>
2019-04-22 12:49:13 +0300

    Working with wordnet similarity function and pre computing data

--- main.py
+++ main.py
@@ -1,92 +1,89 @@
 import itertools
-import matplotlib.pyplot as plt
-import networkx as nx
 import time
+import operator
+import nltk
+import pickle
 from collections import defaultdict
+from nltk.corpus import wordnet as wn
 
+nltk.download('wordnet')
 '''
 Number of applicants: 7428
-Number of skills: 4480
-7428*7427*7426*7425
-3041842488715800
-avg 0.0001 s per team score calc
-304184248871.5800 s = 9639 years
+Number of different skills: 4480
 
 An applicant 'a' is a tuple in the following format:
 a = (identifier, {skill1, skill2, ...})
 
 Likewise, a project 'p' is a tuple in the following format:
 a = (identifier, {skill1, skill2, ...})
-
-Skills of the applicant or project are used in the algorithm
- by their numerical identifiers such as 0,1,2,...
 '''
 set_A = set()
-skills_dict = {}
+distances = {}
 
 
 def get_set_a():
     # Read skills file and create applicants tuples in set_A
-    applicants_data = []
+    global set_A
     with open("DBLP_skill.csv", 'r', encoding='latin1') as temp_f:
         lines = temp_f.readlines()
         for index, line in enumerate(lines):
             split_line = line.rstrip('\n').split(',')
             if len(split_line) > 1:
-                applicants_data.append((index, frozenset(split_line[1:])))
+                set_A.add((index, frozenset(split_line[1:])))
     temp_f.close()
 
     # Gets frequency of terms
     freq_skills = defaultdict(int)
-    for applicant in applicants_data:
+    for applicant in set_A:
         applicant_skills = applicant[1]
         for skill in applicant_skills:
             freq_skills[skill] += 1
 
-    # Builds a skill dictionary with ids instead of the terms for faster processing TODO needed?
-    for index, term in enumerate(freq_skills.keys()):
-        skills_dict[index] = term
-    freq_skills_id = {}
-    for sid, term in skills_dict.items():
-        freq_skills_id[sid] = freq_skills[term]
+    global distances
+    try:
+        print('Found "distances.dat" pre-computed term similarities file.')
+        distances = pickle.load(open("distances.dat", "rb"))
+    except (OSError, IOError) as e:
+        # Pre compute all shortest distances between skills in the tree
+        print('Not found "distances.dat" pre-computed term similarities file. Calculating...')
+        for term1 in freq_skills.keys():
+            print('\tCalculating similarity for term "{}"'.format(term1))
+            for term2 in freq_skills.keys():
+                try:
+                    term1_synset = wn.synset(term1 + '.n.01')
+                    term2_synset = wn.synset(term2 + '.n.01')
+                    score = term1_synset.path_similarity(term2_synset)
+                except nltk.corpus.reader.wordnet.WordNetError:
+                    score = 0
+                if term1 in distances:
+                    distances[term1][term2] = score
+                else:
+                    distances[term1] = {term2: score}
+        print('Done! Saving "distances.dat" pre-computed term similarities file.')
+        pickle.dump(distances, open("distances.dat", "wb"))
 
-    # Recreates applicants_data as set_A with ids instead of strings
-    for applicant in applicants_data:
-        set_A.add((applicant[0], frozenset([list(skills_dict.values()).index(skill) for skill in applicant[1]])))
-
-    # Gets 2k most frequent terms from all the 4480 terms
-    most_freq = sorted(freq_skills_id.items(), key=lambda item: item[1], reverse=True)[:2000]
-
-
-# Creates a tree (graph) with the identifiers of the skills and their relations
-G = nx.random_tree(2000)  # TODO
-
-# Pre compute all shortest distances between skills in the tree
-distances = dict(nx.all_pairs_shortest_path_length(G))
+        # Gets 2k most frequent terms from all the 4480 terms
+        most_freq = sorted(freq_skills.items(), key=lambda item: item[1], reverse=True)[:2000]
 
 
 def get_set_p():
-    set_p = frozenset([
-        ('Project 1', frozenset([list(skills_dict.values()).index(skill) for skill in
-                                 ['learning', 'networks', 'knowledge', 'study', 'graphs', 'analysis']])),
-        ('Project 2', frozenset([list(skills_dict.values()).index(skill) for skill in
-                                 ['learning', 'fuzzy', 'algorithms', 'networks', 'classification', 'analysis']])),
-        ('Project 3', frozenset([list(skills_dict.values()).index(skill) for skill in
-                                 ['graphs', 'clustering', 'distributed', 'algorithms', 'analysis']])),
-        ('Project 4', frozenset([list(skills_dict.values()).index(skill) for skill in
-                                 ['graphs', 'clustering', 'distributed', 'algorithms', 'analysis']])),
-    ])
+    set_p = [
+        ('Project 1', frozenset(['learning', 'networks', 'knowledge', 'study', 'graphs', 'inference', 'analysis'])),
+        ('Project 2', frozenset(['learning', 'fuzzy', 'algorithms', 'networks', 'classification', 'analysis'])),
+        ('Project 3', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis'])),
+        ('Project 4', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+        ('Project 5', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+        ('Project 6', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+        ('Project 7', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+        ('Project 8', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+        ('Project 9', frozenset(['graphs', 'clustering', 'distributed', 'algorithms', 'analysis', 'data'])),
+    ]
     return set_p
 
 
-# Similarity function
+# Similarity function (using pre computed values)
 def similarity(s1, s2):
-    if s1 == s2:
-        return 1
-    elif s1 < 2000 and s2 < 2000 and nx.is_simple_path(G, [s1, s2]):
-        return 1 / distances[s1][s2]
-    else:
-        return 0
+    return distances[s1][s2]
 
 
 # scoreAR(a,r) function
@@ -120,11 +117,8 @@ def brute_force_teams(applicants, projects, k):
         possible_teams = itertools.combinations(applicants, k)
         team_scores = {}
         for team in possible_teams:
-            start = time.time()
             score_team = score_tp(team, project)
             team_scores[team] = score_team
-            start_end = time.time()
-            print(start_end - start)
         best_team_for_project = max(team_scores, key=team_scores.get)
         best_teams[project] = (best_team_for_project, team_scores[best_team_for_project])
         for member in best_team_for_project:
@@ -140,19 +134,19 @@ def single_heuristic_teams(applicants, projects, k):
         for applicant in applicants:
             score = score_ap(applicant, project)
             if project in project_applicants_scores:
-                project_applicants_scores[project].append((applicant, score))
+                project_applicants_scores[project][applicant] = score
             else:
-                project_applicants_scores[project] = [(applicant, score)]
+                project_applicants_scores[project] = {applicant: score}
     for m in range(k):
         for project in projects:
             project_available = project_applicants_scores[project]
-            best_applicant = max(project_available, key=lambda item: item[1])
+            best_applicant = max(project_available.items(), key=operator.itemgetter(1))
             if project in best_teams:
                 best_teams[project].append(best_applicant)
             else:
                 best_teams[project] = [best_applicant]
-            project_applicants_scores = {project: [a for a in lst if a != best_applicant]
-                                         for project, lst in project_applicants_scores.items()}
+            for p_available in project_applicants_scores.values():
+                p_available.pop(best_applicant[0], None)
     return best_teams
 
 
@@ -164,25 +158,24 @@ def pair_heuristic_teams(applicants, projects, k):
         for applicant in applicants:
             score = score_ap(applicant, project)
             if project in project_applicants_scores:
-                project_applicants_scores[project].append((applicant, score))
+                project_applicants_scores[project][applicant] = score
             else:
-                project_applicants_scores[project] = [(applicant, score)]
+                project_applicants_scores[project] = {applicant: score}
     for m in range(0, k, 2):
         for project in projects:
             project_available = project_applicants_scores[project]
-            best_applicant = max(project_available, key=lambda item: item[1])
+            best_applicant = max(project_available.items(), key=operator.itemgetter(1))
             if project in best_teams:
                 best_teams[project].append(best_applicant)
             else:
                 best_teams[project] = [best_applicant]
-            project_applicants_scores = {project: [a for a in lst if a != best_applicant]
-                                         for project, lst in project_applicants_scores.items()}
+            for p_available in project_applicants_scores.values():
+                p_available.pop(best_applicant[0], None)
             if m < k:
-                project_available.remove((best_applicant[0], best_applicant[1]))  # Is this line needed?
-                snd_best_applicant = max(project_available, key=lambda item: item[1])
+                snd_best_applicant = max(project_available.items(), key=operator.itemgetter(1))
                 best_teams[project].append(snd_best_applicant)
-                project_applicants_scores = {project: [a for a in lst if a != snd_best_applicant]
-                                             for project, lst in project_applicants_scores.items()}
+                for p_available in project_applicants_scores.values():
+                    p_available.pop(snd_best_applicant[0], None)
     return best_teams
 
 
@@ -199,9 +192,8 @@ def print_results():
 
     print('Input: --------------')
     for project in set_p:
-        print('\t"{}",\trequirements:"{}"'.format(project[0], [skills_dict[skill] for skill in project[1]]))
+        print('\t"{}",\trequirements:"{}"'.format(project[0], list(project[1])))
     print('\tAmount of applicants: {}'.format(len(set_A)))
-    print('\tAmount of different skills: {}'.format(len(skills_dict)))
 
     print('\nCalculating single heuristic method...')
     start_single_heuristic = time.time()
@@ -209,7 +201,7 @@ def print_results():
     end_single_heuristic = time.time()
     time_single_heuristic = end_single_heuristic - start_single_heuristic
 
-    print('\nCalculating pair heuristic method...')
+    print('Calculating pair heuristic method...')
     start_pair_heuristic = time.time()
     res_ph = pair_heuristic_teams(set_A, set_p, 6)
     end_pair_heuristic = time.time()
@@ -227,25 +219,21 @@ def print_results():
             print('\t\t"{}",\tskills:{},\tscoreAP:{}'
                   .format(member[0], [skills_dict[skill] for skill in member[1]], score_ap(member, project)))
     '''
-    print('Single heuristic: --------------')
+    print('\nSingle heuristic: --------------')
     print('Execution time:\t{}'.format(time_single_heuristic))
     for project, team in res_sh.items():
-        print('\t->"{}":\tscoreTP:{},\trequirements:{}'
-              .format(project[0], score_tp([m[0] for m in team], project),
-                      [skills_dict[skill] for skill in project[1]]))
+        print('\t->"{0!s}":\tscoreTP:{1:.3f},\trequirements:{2!s}'
+              .format(project[0], score_tp([m[0] for m in team], project), project[1]))
         for member in team:
-            print('\t\t-{}:\tscoreAP:{}\tskills:{}'
-                  .format(member[0][0], member[1], [skills_dict[skill] for skill in member[0][1]]))
+            print('\t\t->Applicant {0!s}:\tscoreAP:{1:.3f}\tskills:{2!s}'.format(member[0][0], member[1], member[0][1]))
 
-    print('Pair heuristic: ----------------')
+    print('\nPair heuristic: ----------------')
     print('Execution time:\t{}'.format(time_pair_heuristic))
     for project, team in res_ph.items():
-        print('\t->"{}":\tscoreTP:{},\trequirements:{}'
-              .format(project[0], score_tp([m[0] for m in team], project),
-                      [skills_dict[skill] for skill in project[1]]))
+        print('\t->"{0!s}":\tscoreTP:{1:.3f},\trequirements:{2!s}'
+              .format(project[0], score_tp([m[0] for m in team], project), project[1]))
         for member in team:
-            print('\t\t-{}:\tscoreAP:{}\tskills:{}'
-                  .format(member[0][0], member[1], [skills_dict[skill] for skill in member[0][1]]))
+            print('\t\t->Applicant {0!s}:\tscoreAP:{1:.3f}\tskills:{2!s}'.format(member[0][0], member[1], member[0][1]))
 
 
 # Execute algorithms and print results
